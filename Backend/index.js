f011957124177b1f555fcc11ab554c16bee51966
Anurag Yadav <10anuragyadav2002@gmail.com>
2024-09-04 16:12:21 +0530

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ import userRoute from "./route/user.route.js";
 const app = express();
 
 app.use(cors({
-    origin:["http://localhost:5173","https://bookStore.web.app"],
+    origin:["http://localhost:5173","https://bookstore-52cca.web.app"],
     methods:['POST','GET','HEAD','PUT','DELETE'],
     credentials: true
 }));
