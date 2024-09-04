3bb64c4b185b3f85608865ed2b2ac6216baf09fa
Anurag Yadav <10anuragyadav2002@gmail.com>
2024-09-04 14:38:24 +0530

    cors updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
+
 import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
@@ -8,29 +10,46 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173/","https://bookStore.web.app"],
+    methods:['POST','GET','HEAD','PUT','DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 
-dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+const URI = process.env.MONGO_URI;
+
+console.log("Mongodb uri",URI)
 
 // connect to mongoDB
 
 
 // Connect to MongoDB
-const connectDB = async () => {
-    try {
-        // Connect to MongoDB
-         mongoose.connect(URI)
-        // Log successful connection
-          console.log("Connected to mongoDB");
-    } catch (error) {
-    console.log("Error: ", error);
-}
-}
-connectDB();
+// const connectDB = async () => {
+//     try {
+//         // Connect to MongoDB
+//          mongoose.connect(URI)
+//         // Log successful connection
+//           console.log("Connected to mongoDB");
+//     } catch (error) {
+//     console.log("Error: ", error);
+// }
+// }
+
+const connectDatabase = () => {
+    mongoose
+      .connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((data) => {
+        console.log("Database connected");
+      });
+  };
+  
+  connectDatabase();
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
