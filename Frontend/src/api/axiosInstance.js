d5a99eaa7fd3f5b777afddc04148d763df717192
Anurag Yadav <10anuragyadav2002@gmail.com>
2024-10-27 02:15:35 +0530

    baseURL updated in api frontend

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://book-store-application-tzan.onrender.com",
+  baseURL: "https://book-store-application-tzan.onrender.com",
   // baseURL: "http://localhost:4001",
   headers: {
     "Content-Type": "application/json",
